2aa21e9db98a1950ddaa3f492c68d7331146c009
Travis Schettler <travis_schettler@hotmail.com>
2018-03-21 00:44:46 -0500

    Removed unnecessary dependencies

--- LogTesting/TestLogModelAppender.cs
+++ LogTesting/TestLogModelAppender.cs
@@ -1,14 +1,13 @@
 ﻿using Log4Net.EntityLogging;
 using System.Collections.Generic;
-using System.Data.Common;
 
 namespace LogTesting
 {
     public class TestLogModelAppender : EntityAppender<TestLogModel>
     {
-        protected override void Save(DbTransaction transaction, List<TestLogModel> items)
+        protected override void Save(List<TestLogModel> items)
         {
-            base.Save(transaction, items);
+            base.Save(items);
         }
     }
 }
